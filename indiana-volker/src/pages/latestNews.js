b9b25be9f796116642b4e20b0e0aec7d5f55e17b
Francisco Torres <fco.lt94@gmail.com>
2020-05-10 21:20:42 -0500

    primerosEstilos

---
+++ indiana-volker/src/pages/latestNews.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './latestNews.css';
+
+function latestNews(){
+    return(
+    <section class="latestnews">
+        <div class="item">Balanza I</div>
+        <div class="item">Balanza II</div>
+        <div class="item">Mc Carthys</div>
+    </section>
+    )
+}
+
+
+export default latestNews;
