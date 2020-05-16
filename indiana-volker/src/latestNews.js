a473784dacf975b6e894ce6042537c9fbd4624af
Hugo Reyes <hugo.reyes.casanova@gmail.com>
2020-05-15 20:06:47 -0500

    Regresando la parte de LatestNews de Pancho a como él lo dejó

--- indiana-volker/src/latestNews.js
+++ indiana-volker/src/latestNews.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import './latestNews.css';
+import './LatestNews.css';
 
 function latestNews(){
   return (
     <>
        <h1><strong>Últimos Lanzamientos</strong></h1>
-       <section className="reciente">
-          <div className="contenedor">
+       <section class="reciente">
+          <div class="contenedor">
              <img src="/assets/Balanza 1_upload.jpg" alt="Foto de portada del albúm Balanza 1" />
-               <a className="news-link">Ir Al Álbum</a>
+             <p><strong>Ir al albúm</strong></p>
           </div>
-          <div className="contenedor">
+          <div class="contenedor">
              <img src="/assets/Balanza 2_upload.jpg" alt="Foto de portada del albúm Balanza 2" />
-             <a className="news-link">Ir Al Álbum</a>
+             <p><strong>Ir al albúm</strong></p>
           </div>
-          <div className="contenedor">
+          <div class="contenedor">
              <img src="/assets/IV - McCarthys (14 of 38).jpg" alt="Foto de toquin en bar McCarthys" />
-             <a className="news-link">McCarthys</a>
+             <p><strong>McCarthys</strong></p>
           </div>
        </section>
     </>
