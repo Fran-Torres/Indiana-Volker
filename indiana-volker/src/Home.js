899ece915c30a08cbe0b446fae02f0d8ff2bb9a9
Francisco Torres <fco.lt94@gmail.com>
2020-05-15 21:17:28 -0500

    Union de Branches/Home page

--- indiana-volker/src/Home.js
+++ indiana-volker/src/Home.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import './latestNews.css';
+import './Home.css';
+import fluid from "react-bootstrap";
+import Image from 'react-bootstrap/Image';
 
-function latestNews(){
+function Home(){
   return (
     <>
+       <div className="bannerselector" id="main-banner" >
+            <img className="banner" alt="banner" src="./assets/Indiana Volker v2.jpg" fluid />
+          </div>
        <h1><strong>Últimos Lanzamientos</strong></h1>
        <section className="reciente">
           <div className="contenedor">
              <img src="/assets/Balanza 1_upload.jpg" alt="Foto de portada del albúm Balanza 1" />
-               <a className="news-link">Ir Al Álbum</a>
+               <a className="news-link">Ir al Álbum</a>
           </div>
           <div className="contenedor">
              <img src="/assets/Balanza 2_upload.jpg" alt="Foto de portada del albúm Balanza 2" />
-             <a className="news-link">Ir Al Álbum</a>
+             <a className="news-link">Ir al Álbum</a>
           </div>
           <div className="contenedor">
              <img src="/assets/IV - McCarthys (14 of 38).jpg" alt="Foto de toquin en bar McCarthys" />
@@ -23,4 +28,4 @@ function latestNews(){
   );
 };
 
-export default latestNews;
+export default Home;
