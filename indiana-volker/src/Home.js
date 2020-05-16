416dd7cc3e24b2bb4c009963758969804fba6479
Francisco Torres <fco.lt94@gmail.com>
2020-05-16 11:54:39 -0500

    Diseño Responsivo en "últmos lanzamientos", cambios a estilos en Home.css y nav-footer.css

--- indiana-volker/src/Home.js
+++ indiana-volker/src/Home.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import './Home.css';
+import { Row, Col } from 'reactstrap'
 import fluid from "react-bootstrap";
 import Image from 'react-bootstrap/Image';
 
-function Home(){
-  return (
-    <>
-       <div className="bannerselector" id="main-banner" >
+function Home() {
+   return (
+      <>
+         <div className="bannerselector" id="main-banner" >
             <img className="banner" alt="banner" src="./assets/Indiana Volker v2.jpg" fluid />
-          </div>
-       <h1><strong>Últimos Lanzamientos</strong></h1>
-       <section className="reciente">
-          <div className="contenedor">
-             <img src="/assets/Balanza 1_upload.jpg" alt="Foto de portada del albúm Balanza 1" />
-               <a id="Berni"><strong>Ir al Álbum</strong></a>
-          </div>
-          <div className="contenedor">
-             <img src="/assets/Balanza 2_upload.jpg" alt="Foto de portada del albúm Balanza 2" />
-             <a id="Taylor"><strong>Ir al Álbum</strong></a>
-          </div>
-          <div className="contenedor">
-             <img src="/assets/IV - McCarthys (14 of 38).jpg" alt="Foto de toquin en bar McCarthys" />
-             <a id="Ibarra"><strong>McCarthys</strong></a>
-          </div>
-       </section>
-    </>
-  );
+         </div>
+         <h1><strong>Últimos Lanzamientos</strong></h1>
+         <section className="reciente">
+            <Row style={{textAlign:'center'}}>
+               <Col style={{textAlign:'center'}}>
+                  <div className="contenedor">
+                     <img src="/assets/Balanza 1_upload.jpg" alt="Foto de portada del albúm Balanza 1" />
+                     <a id="Berni"><strong>Ir al Álbum</strong></a>
+                  </div>
+               </Col>
+               <Col>
+                  <div className="contenedor">
+                     <img src="/assets/Balanza 2_upload.jpg" alt="Foto de portada del albúm Balanza 2" />
+                     <a id="Taylor"><strong>Ir al Álbum</strong></a>
+                  </div>
+               </Col>
+               <Col>
+                  <div className="contenedor">
+                     <img src="/assets/IV - McCarthys (14 of 38).jpg" alt="Foto de toquin en bar McCarthys" />
+                     <a id="Ibarra"><strong>McCarthys</strong></a>
+                  </div>
+               </Col>
+            </Row>
+         </section>
+      </>
+   );
 };
 
 export default Home;
