95296ad9ed1fcff0d294fc4bd0c3eb112c002c21
Bollom <cobygonza@gmail.com>
2020-05-17 22:36:12 -0500

    Links to
    Agregamos las rutas, links, paginas de albumes y estilos. Error en que se carga la pagina de Home y empieza a compilar debajo la nueva pagina

---
+++ indiana-volker/src/Pages/Balanza2.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../components/Navbar.js';
+import Footer from '../components/Footer.js';
+import { Redirect, Link, Router, Route } from 'react-router-dom';
+import './Estilos-albumes.css';
+import { Row, Col } from 'reactstrap';
+
+
+export default function Balanza2() {
+    return (
+        <>
+            <Navbar />
+            <Row>
+                <div className="Balanza2estilos">
+                </div>
+            </Row>
+            <Footer />
+        </>
+    )
+}
---
+++ indiana-volker/src/components/Step3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Pages/Home.css';
+
+
+export default function step3() {
+    return (
+        <div className="contenedor">
+                     <img src="/assets/IV - McCarthys (14 of 38).jpg" alt="Foto de toquin en bar McCarthys" />
+                     <a id="Ibarra"><strong>McCarthys</strong></a>
+                  </div>
+    )
+}
