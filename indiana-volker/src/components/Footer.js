414e51151ac41df28b532d9541275b4a5d725115
Bollom <cobygonza@gmail.com>
2020-05-18 20:54:59 -0500

    agregamos link a las redes sociales
    Lo que dice arriba

--- indiana-volker/src/components/Footer.js
+++ indiana-volker/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 export default function Footer() {
   return (
@@ -7,10 +8,10 @@ export default function Footer() {
     <div className="social-media" id="social-media">
       <FontAwesomeIcon icon={['fab', 'youtube']} className="social-media-icon"/>
       <FontAwesomeIcon icon={['fab', 'twitter']} className="social-media-icon"/>
-      <FontAwesomeIcon icon={['fab', 'spotify']} className="social-media-icon"/>
-      <FontAwesomeIcon icon={['fab', 'instagram']} className="social-media-icon"/>
+      <a href="https://open.spotify.com/artist/6TBGcnLbwLJUJVjJojqg0a?si=t2wAjLQgRKanQfI3PyehEQ"><FontAwesomeIcon icon={['fab', 'spotify']} className="social-media-icon"/></a>
+      <a href="https://www.instagram.com/indianavolker/"> <FontAwesomeIcon icon={['fab', 'instagram']} className="social-media-icon"/></a>
       <FontAwesomeIcon icon="envelope" className="social-media-icon" />
-      <FontAwesomeIcon icon={['fab', 'facebook']} className="social-media-icon"/>
+      <a href="https://www.facebook.com/indianavolker"> <FontAwesomeIcon icon={['fab', 'facebook']} className="social-media-icon"/></a>
     </div>
     <footer>
       <p id="copyright">Copyright Indiana Volker 2020</p>
