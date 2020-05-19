7385fd20d9e01c6b5fc8dc88a190151d72487f93
Bollom <cobygonza@gmail.com>
2020-05-18 20:37:34 -0500

    Arreglo Link y Paginas
    Arreglamos los Links y las Paginas de los albumes

--- indiana-volker/src/Pages/McCarthys.js
+++ indiana-volker/src/Pages/McCarthys.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Navbar from '../components/Navbar.js';
 import Footer from '../components/Footer.js';
+import { Redirect, Link, Router, Route } from 'react-router-dom';
+import './Estilos-albumes.css';
+import { Row, Col } from 'reactstrap';
 
 
 export default function McCarthys() {
     return (
         <>
-        <Navbar/>
-        <Footer/>
+         
+            <Row>
+                <div className="Balanza2estilos">
+                </div>
+            </Row>
+          
         </>
-        )     
+    )
 }
--- indiana-volker/src/components/Navbar.js
+++ indiana-volker/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Navbar-footer.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
       <FontAwesomeIcon icon="bars" id="hamburguer-menu" className="icon" />
-      <img src={require ("../assets/Logo_indiana_volker_fondo_transaprente_Big.png") } id="nav-logo" alt="Indiana Volker Logo"/>
+      <Link to ="/"> <img src={require ("../assets/Logo_indiana_volker_fondo_transaprente_Big.png") } id="nav-logo" alt="Indiana Volker Logo"/> </Link>
       <FontAwesomeIcon icon="shopping-cart" id="shopping-cart" className="icon" />
     </nav>
   )
